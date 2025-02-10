3cf01a9294a83b1191ae70aea2eb76d9532a5a03
annesura <annesura00@gmail.com>
2025-02-10 18:21:26 -0500

    Added basic HTML structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Weather from "./Weather.js";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+      <Weather />
       <footer>This project was built by Anne Sura and is open-sourced on <a href="https://github.com/annesura/react-weather-project" target="_blank">GitHub</a>.</footer>
+      </div>
     </div>
   );
 }
