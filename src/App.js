150b25feed2e887681e49f5faa1878169192270e
annesura <annesura00@gmail.com>
2025-02-10 17:42:47 -0500

    Added footer with link to GitHub repository.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>This project was built by Anne Sura and is open-sourced on <a href="https://github.com/annesura/react-weather-project" target="_blank">GitHub</a>.</footer>
     </div>
   );
 }
