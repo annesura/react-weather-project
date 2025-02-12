0c03df939baf9c45f53c6f470d24d0f5d9ebe129
annesura <annesura00@gmail.com>
2025-02-12 10:54:31 -0500

    Added search functionality

--- src/Weather.js
+++ src/Weather.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate";
+import WeatherInfo from "./WeatherInfo";
 import axios from "axios";
 
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState({ ready: false});
+    const [city, setCity] = useState(props.defaultCity);
 
     function handleResponse(response) {
         console.log(response.data);
@@ -18,51 +19,44 @@ export default function Weather(props) {
             humidity: response.data.temperature.humidity,
             wind: response.data.wind.speed,
         })
+    }
+
+    function searchCity() {
+        const apiKey = "0cca9cddf1f4t4bb307e8bfo1fa213f2";
+        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        searchCity();
 
     }
 
-    
+    function handleChange(event) {
+        setCity(event.target.value);
+    }
 
 
     if (weatherData.ready) {
         return (
         <div className="Weather">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="row">
                     <div className="col-9">
-                <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" />
+                <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on" onChange={handleChange} />
                 </div>
                 <div className="col-3">
                 <input type="submit" value="Search" className="btn btn-primary w-100" />
                 </div>
                 </div>
             </form>
-            <h1>{weatherData.city}</h1>
-            <ul>
-                <li>
-                    <FormattedDate date={weatherData.date} />
-                </li>
-                <li className="text-capitalize">{weatherData.description}</li>
-            </ul>
-            <div className="row description">
-                <div className="col-6">
-                    <img src={weatherData.icon} alt={weatherData.description} className="icon" />
-                   <span className="temperature">{Math.round(weatherData.temperature)}</span> <span className="unit">°F</span>
-                </div>
-                <div className="col-6">
-                    <ul>
-                        <li>Humidity: {weatherData.humidity}%</li>
-                        <li>Wind: {Math.round(weatherData.wind)} mph</li>
-                    </ul>
-                </div>
-            </div>
+            <WeatherInfo data={weatherData} />
         </div>
         )
     }
     else {
-        const apiKey = "0cca9cddf1f4t4bb307e8bfo1fa213f2";
-        let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=imperial`;
-        axios.get(apiUrl).then(handleResponse);
+        searchCity();
 
         return "Loading. . .";
     }
---
+++ src/WeatherInfo.js
@@ -0,0 +1,28 @@
+import React from "react";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherInfo(props) {
+    return (
+    <div className="WeatherInfo">
+        <h1>{props.data.city}</h1>
+            <ul>
+                <li>
+                    <FormattedDate date={props.data.date} />
+                </li>
+                <li className="text-capitalize">{props.data.description}</li>
+            </ul>
+            <div className="row description">
+                <div className="col-6">
+                    <img src={props.data.icon} alt={props.data.description} className="icon" />
+                   <span className="temperature">{Math.round(props.data.temperature)}</span> <span className="unit">°F</span>
+                </div>
+                <div className="col-6">
+                    <ul>
+                        <li>Humidity: {props.data.humidity}%</li>
+                        <li>Wind: {Math.round(props.data.wind)} mph</li>
+                    </ul>
+                </div>
+            </div>
+    </div>
+    )
+}
